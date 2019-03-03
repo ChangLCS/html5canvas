8e11a1f76f0e0d4ea845e7f25b370fe103799b56
Chang <584911924@qq.com>
2019-03-03 22:23:50 +0800

    完成第一章的学习，可能还有一些不太明白的地方，已经完成星空背景以及玩家飞船的初始化

--- Alien Invasion/engine.js
+++ Alien Invasion/engine.js
@@ -1,3 +1,67 @@
+//  初始化游戏对象 Game
+var Game = new function() {
+  this.initialize = function(canvasElementId, sprite_data, callback) {
+    this.canvas = document.getElementById(canvasElementId);
+    this.width = this.canvas.width;
+    this.height = this.canvas.height;
+
+    //  初始化canvas对象的ctx
+    this.ctx = this.canvas.getContext && this.canvas.getContext('2d');
+
+    //  配置键盘监听
+    this.setupInput();
+
+    //  开始游戏的循环
+    this.loop();
+
+    //  加入图片资源
+    SpriteSheet.load(sprite_data, callback);
+  };
+
+  //  需要监听的键盘输入
+  var KEY_CODES = {
+    37: 'left',
+    39: 'right',
+    32: 'fire',
+  };
+  this.keys = {};
+  this.setupInput = function() {
+    //  按下键盘的时候
+    window.addEventListener('keydown', function(e) {
+      if (KEY_CODES[e.keyCode]) {
+        Game.keys[KEY_CODES[e.keyCode]] = true;
+        e.preventDefault();
+      }
+    });
+    //  放开键盘的时候
+    window.addEventListener('keyup', function(e) {
+      if (KEY_CODES[e.keyCode]) {
+        Game.keys[KEY_CODES[e.keyCode]] = false;
+        e.preventDefault();
+      }
+    });
+  };
+
+  //  游戏的循环事件
+  var boards = [];
+  this.loop = function() {
+    var dt = 30 / 1000;
+    for (let i = 0; i < boards.length; i += 1) {
+      const item = boards[i];
+      if (item) {
+        item.step(dt);
+        item && item.draw(Game.ctx);
+      }
+    }
+    setTimeout(Game.loop, 30);
+  };
+
+  //  修改当前激活的绘制板
+  this.setBoard = function(num, board) {
+    boards[num] = board;
+  };
+}();
+
 //  精灵图加载对象
 var SpriteSheet = new function() {
   this.map = {};
--- Alien Invasion/game.js
+++ Alien Invasion/game.js
@@ -1,31 +1,155 @@
-var canvas = document.getElementById('game');
+// var canvas = document.getElementById('game');
 
-var ctx = canvas.getContext('2d');
-startGame();
+// var ctx = canvas.getContext('2d');
+// startGame();
 
-//  开始游戏
-function startGame() {
-  ctx.fillStyle = '#ffff00';
-  ctx.fillRect(50, 100, 380, 400);
+// //  开始游戏
+// function startGame() {
+//   ctx.fillStyle = '#ffff00';
+//   ctx.fillRect(50, 100, 380, 400);
 
-  //  填充一个蓝色的半透明区块
-  ctx.fillStyle = 'rgba(0,0,128,.8)';
-  ctx.fillRect(25, 50, 380, 400);
+//   //  填充一个蓝色的半透明区块
+//   ctx.fillStyle = 'rgba(0,0,128,.8)';
+//   ctx.fillRect(25, 50, 380, 400);
 
-  SpriteSheet.load(
-    {
-      ship: {
-        sx: 0,
-        sy: 0,
-        w: 37,
-        h: 42,
-        frame: 1,
-      },
-    },
-    function() {
-      SpriteSheet.draw(ctx, 'ship', 0, 0);
-      SpriteSheet.draw(ctx, 'ship', 100, 50);
-      SpriteSheet.draw(ctx, 'ship', 150, 100, 1);
-    },
-  );
-}
+//   SpriteSheet.load(
+//     {
+//       ship: {
+//         sx: 0,
+//         sy: 0,
+//         w: 37,
+//         h: 42,
+//         frame: 1,
+//       },
+//     },
+//     function() {
+//       SpriteSheet.draw(ctx, 'ship', 0, 0);
+//       SpriteSheet.draw(ctx, 'ship', 100, 50);
+//       SpriteSheet.draw(ctx, 'ship', 150, 100, 1);
+//     },
+//   );
+// }
+
+var sprites = {
+  ship: { sx: 0, sy: 0, w: 39, h: 42, frames: 1 },
+};
+
+var startGame = function() {
+  // SpriteSheet.draw(Game.ctx, 'ship', 100, 100, 1);
+  Game.setBoard(0, new Starfield(20, 0.4, 100, true));
+  Game.setBoard(1, new Starfield(50, 0.6, 100));
+  Game.setBoard(2, new Starfield(100, 1, 50));
+  Game.setBoard(3, new TitleScreen('Alien Invasion', 'Press space to start playing', playGame));
+};
+
+var playGame = function() {
+  Game.setBoard(3, new PlayerShip());
+};
+
+window.addEventListener('load', function() {
+  Game.initialize('game', sprites, startGame);
+});
+
+//  星空背景
+var Starfield = function(speed, opacity, numStarts, clear) {
+  //  初始化背景的canvas
+  var stars = document.createElement('canvas');
+  stars.width = Game.width;
+  stars.height = Game.height;
+
+  var starCtx = stars.getContext('2d');
+  var offset = 0;
+
+  //  clear 如果存在，则背景是黑色的，而不是透明了
+  if (clear) {
+    starCtx.fillStyle = '#000';
+    starCtx.fillRect(0, 0, stars.width, stars.height);
+  }
+
+  //  画两个像素的白色，当作星星
+  starCtx.fillStyle = '#fff';
+  starCtx.globalAlpha = opacity;
+  for (let i = 0; i < numStarts; i += 1) {
+    starCtx.fillRect(
+      Math.floor(Math.random() * stars.width),
+      Math.floor(Math.random() * stars.height),
+      2,
+      2,
+    );
+  }
+
+  this.draw = function(ctx) {
+    var intOffset = Math.floor(offset);
+    var remaining = stars.height - intOffset;
+    // console.log('intOffset', intOffset, 'remaining', remaining);
+
+    //  这两段画画只是为了滚动效果，使得一出来就有星星能画
+    //  这个画星空的下半部分
+    if (intOffset > 0) {
+      ctx.drawImage(stars, 0, remaining, stars.width, intOffset, 0, 0, stars.width, intOffset);
+    }
+    //  这个画星空的上半部分
+    if (remaining > 0) {
+      ctx.drawImage(stars, 0, 0, stars.width, remaining, 0, intOffset, stars.width, remaining);
+    }
+  };
+
+  this.step = function(dt) {
+    offset += dt * speed;
+    offset = offset % stars.height;
+  };
+};
+
+//  标题
+var TitleScreen = function(title, subtitle, callback) {
+  this.step = function(dt) {
+    if (Game.keys['fire'] && callback) callback();
+  };
+
+  this.draw = function(ctx) {
+    ctx.fillStyle = '#fff';
+    ctx.textAlign = 'center';
+    //  大标题
+    ctx.font = 'bold 40px bangers';
+    ctx.fillText(title, Game.width / 2, Game.height / 2);
+    //  副标题
+    ctx.font = 'bold 20px bangers';
+    ctx.fillText(subtitle, Game.width / 2, Game.height / 2 + 40);
+  };
+};
+
+//  玩家飞船
+var PlayerShip = function() {
+  this.w = SpriteSheet.map.ship.w;
+  this.h = SpriteSheet.map.ship.h;
+  this.x = Game.width / 2 - this.w / 2;
+  this.y = Game.height - 10 - this.h;
+  this.vx = 0;
+
+  this.step = function(dt) {
+    //  玩家的控制
+    this.maxVel = 200;
+    this.step = function(dt) {
+      if (Game.keys['left']) {
+        this.vx = -this.maxVel;
+      } else if (Game.keys['right']) {
+        this.vx = this.maxVel;
+      } else {
+        this.vx = 0;
+      }
+
+      this.x += this.vx * dt;
+
+      //  防止飞船超出canvas边界
+      if (this.x < 0) {
+        this.x = 0;
+      } else if (this.x > Game.width - this.w) {
+        this.x = Game.width - this.w;
+      }
+    };
+  };
+
+  this.draw = function(ctx) {
+    SpriteSheet.draw(ctx, 'ship', this.x, this.y, 1);
+  };
+};
